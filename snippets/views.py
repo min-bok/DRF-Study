b44540d5832768579d4ec636bc2cbc9af9a726ec
min-bok <leehy0782@gmail.com>
2023-08-22 18:00:08 +0900

    230822

--- snippets/views.py
+++ snippets/views.py
@@ -4,5 +4,16 @@ from rest_framework.parsers import JSONParser
 from .models import Snippet
 from .serializers import SnippetSerializer
 
-# csrf_exempt
-# api를 만들경우 csrf 보안이 필요하지않으므로, csrf를 꺼주는 전처리기
+@csrf_exempt
+def snippet_list(request):
+    if request.method == "GET":
+        snippets = Snippet.objects.all()
+        serializer = SnippetSerializer(snippets, many=True)
+        return JsonResponse(serializer.data, safe=False)
+    elif request.method == 'POST':
+        data = JSONParser().parse(request)
+        serializer = SnippetSerializer(data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data, status=201)
+        return JsonResponse(serializer.errors, status=400)
